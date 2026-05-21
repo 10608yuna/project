f151f33fc1dd57b26f85e4168f503d48d023e79a
10614eunkyo <dg2026614@danggok.hs.kr>
2026-05-21 09:06:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ Web VPython 3.2
 box(size = vec(160,90,3),color=vec((255)/255, (133)/255, (144)/255))
 box(size = vec(130,75,5),pos = vec (-6.5,1.5,0))
 box(size = vec(135,80,4.5),pos = vec (-6.5,1.5,0),color=vec((182)/255, (44)/255, (55)/255))
-body=cylinder(pos = vec(200,10,10),size = vec(30,7,7),axis=vec(0, 1, 0))
-eye=sphere(pos = vec(200,10,10),color=vec((0)/255, (0)/255, (0)/255),pos = vec(202,34,13))
-eyet=sphere(pos = vec(200,10,10),color=vec((0)/255, (0)/255, (0)/255),pos = vec(198,34,13))
-a=ellipsoid(pos=vec(203,32,13), length=3.5, height=2, width=2, color=vec((249)/255, (165)/255, (158)/255))
-b=ellipsoid(pos=vec(197,32,13), length=3.5, height=2, width=2, color=vec((249)/255, (165)/255, (158)/255))
+body=cylinder(pos = vec(200,10,8),size = vec(30,7,7),axis=vec(0, 1, 0))
+eye=sphere(pos = vec(200,10,8),color=vec((0)/255, (0)/255, (0)/255),pos = vec(202,34,13))
+eyet=sphere(pos = vec(200,10,8),color=vec((0)/255, (0)/255, (0)/255),pos = vec(198,34,13))
+a=ellipsoid(pos=vec(203,32,11), length=3.5, height=2, width=2, color=vec((249)/255, (165)/255, (158)/255))
+b=ellipsoid(pos=vec(197,32,11), length=3.5, height=2, width=2, color=vec((249)/255, (165)/255, (158)/255))
 bunpel = compound([body,eye,eyet,a,b])
 
 follower=sphere
