b7b49775118f8b6e5bd14c18f2d15369a95d5f49
10614eunkyo <dg2026614@danggok.hs.kr>
2026-05-22 09:28:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ eye=sphere(pos = vec(200,10,8),color=vec((0)/255, (0)/255, (0)/255),pos = vec(20
 eyet=sphere(pos = vec(200,10,8),color=vec((0)/255, (0)/255, (0)/255),pos = vec(198,34,13))
 a=ellipsoid(pos=vec(203,32,11), length=3.5, height=2, width=2, color=vec((249)/255, (165)/255, (158)/255))
 b=ellipsoid(pos=vec(197,32,11), length=3.5, height=2, width=2, color=vec((249)/255, (165)/255, (158)/255))
-bunpel = compound([body,eye,eyet,a,b])
-bunpel = pos=vector(0, 0, 0)
+bunpel = compound([body,eye,eyet,a,b], pos=vector(0, 2, 0))
 
 follower=sphere
 
