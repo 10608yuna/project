7ca9f553463db9d359261cbcbfaddbd3bea7bf1d
10614eunkyo <dg2026614@danggok.hs.kr>
2026-05-22 09:36:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ Web VPython 3.2
 box(size = vec(160,90,3),color=vec((255)/255, (133)/255, (144)/255))
 box(size = vec(130,75,5)compound([body, head], pos=vector(0, 0, 0)))
 box(size = vec(135,80,4.5),pos = vec (-6.5,1.5,0),color=vec((182)/255, (44)/255, (55)/255))
+
+==== 분필이 ===
 body=cylinder(pos = vec(200,10,8),size = vec(30,7,7),axis=vec(0, 1, 0))
 eye=sphere(pos = vec(200,10,8),color=vec((0)/255, (0)/255, (0)/255),pos = vec(202,34,13))
 eyet=sphere(pos = vec(200,10,8),color=vec((0)/255, (0)/255, (0)/255),pos = vec(198,34,13))
 a=ellipsoid(pos=vec(203,32,11), length=3.5, height=2, width=2, color=vec((249)/255, (165)/255, (158)/255))
 b=ellipsoid(pos=vec(197,32,11), length=3.5, height=2, width=2, color=vec((249)/255, (165)/255, (158)/255))
-bunpel = compound([body,eye,eyet,a,b], pos=vector(-90, 0, 6))
+ink=box(pos = vec(200,11,10), color=vec((0)/255, (0)/255, (0)/255))
+bunpel = compound([body,eye,eyet,a,b,ink], pos=vector(-90, 0, 6))
 
-follower=sphere
-
+=== 움직임 ===
 while True :
     rate(100)
     k = keysdown()
@@ -22,3 +24,10 @@ while True :
         bunpel.pos.y = bunpel.pos.y + 0.5
     if 's' in k : 
         bunpel.pos.y = bunpel.pos.y + -0.5
+
+==== 선 그리기 ===
+while True :
+    rate(100)
+    k = keysdown()
+    if ' ' in k :
+        
