ede078fb43b1738b3f6a87cbe43bc402a1d13f4b
10614eunkyo <dg2026614@danggok.hs.kr>
2026-05-22 09:51:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ box(size = vec(130,75,5)compound([body, head], pos=vector(0, 0, 0)))
 box(size = vec(135,80,4.5),pos = vec (-6.5,1.5,0),color=vec((182)/255, (44)/255, (55)/255))
 
 ==== 분필이 ===
-body=cylinder(pos = vec(200,10,8),size = vec(30,7,7),axis=vec(0, 1, 0))
-eye=sphere(pos = vec(200,10,8),color=vec((0)/255, (0)/255, (0)/255),pos = vec(202,34,13))
-eyet=sphere(pos = vec(200,10,8),color=vec((0)/255, (0)/255, (0)/255),pos = vec(198,34,13))
-a=ellipsoid(pos=vec(203,32,11), length=3.5, height=2, width=2, color=vec((249)/255, (165)/255, (158)/255))
-b=ellipsoid(pos=vec(197,32,11), length=3.5, height=2, width=2, color=vec((249)/255, (165)/255, (158)/255))
-ink=box(pos = vec(200,10.3,10),size = vec(4,4,4),color=vec((0)/255, (0)/255, (0)/255),(make_trail=True))
+bunpel = group()
+
+body=cylinder(pos = vec(200,10,10),size = vec(30,7,7),axis=vec(0, 1, 0),group=bunpel)
+eye=sphere(pos = vec(200,10,10),color=vec((0)/255, (0)/255, (0)/255),pos = vec(202,34,13),group=bunpel)
+eyet=sphere(pos = vec(200,10,10),color=vec((0)/255, (0)/255, (0)/255),pos = vec(198,34,13),group=bunpel)
+a=ellipsoid(pos=vec(203,32,13), length=3.5, height=2, width=2, color=vec((249)/255, (165)/255, (158)/255),group=bunpel)
+b=ellipsoid(pos=vec(197,32,13), length=3.5, height=2, width=2, color=vec((249)/255, (165)/255, (158)/255),group=bunpel)
+ink=box(pos = vec(200,10.3,10),size = vec(4,4,4),color=vec((0)/255, (0)/255, (0)/255),(make_trail=True),group=bunpel)
+
 bunpel = compound([body,eye,eyet,a,b,ink], pos=vector(-90, 0, 6))
 
 === 움직임 ===
